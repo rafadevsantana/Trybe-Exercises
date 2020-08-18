6bf6af8054c9ef6c4f543b6ede0de0767eb8f52b
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-08-18 15:02:00 -0300

    nbranch

--- exercise6.sh
+++ exercise6.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+
+[200~ FILE=$1
+
+  if [ -f "$FILE" ]
+	      then
+		            echo "$FILE é um arquivo comum"
+			      elif [ -d "$FILE" ]
+				          then
+						        echo "$FILE é um diretório"
+							  else
+								 echo "$FILE é alguma outra coisa"
+									  fi
+									    ls -l $FILE
