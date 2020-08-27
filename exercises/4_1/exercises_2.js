15d8d0dd862fd200c296169f4b618a7d52d854b4
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-08-27 18:17:35 -0300

    primeiros 4 exercicios

---
+++ exercises/4_1/exercises_2.js
@@ -0,0 +1,11 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+const x = 66;
+const y = 37;
+
+if ( x > y){
+    console.log(`a variavel x ${x} é maior que a variavel y ${y} `)
+}
+else {
+    console.log("a variavel x é menor")
+}
