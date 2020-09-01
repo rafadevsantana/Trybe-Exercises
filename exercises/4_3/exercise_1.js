f0c5de3330342fc5829384e0f3a50980566f9979
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-01 14:57:12 -0300

    exercicios asters

---
+++ exercises/4_3/exercise_1.js
@@ -0,0 +1,13 @@
+let n = 5;
+let aster = '*';
+let saidaAster = '';
+
+
+for (let contador = 0;contador < n;contador+=1){
+    saidaAster = saidaAster + aster;
+};
+  
+
+for (contador = 0; contador < n; contador +=1){
+    console.log(saidaAster)
+}
---
+++ exercises/4_3/exercise_2.js
@@ -0,0 +1,8 @@
+let n = 5;
+let aster = '*';
+let linhaAster = '';
+
+for (i = 0;i <= n;i += 1){
+linhaAster += aster
+console.log(linhaAster)
+}
